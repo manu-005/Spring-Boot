7124035e2f5bfcedcc50e0722ee3b15e2daa4aa6
manu-005 <manojbetadur@gmail.com>
2026-06-18 00:01:59 +0530

    get all customers

--- eCommerce/src/main/java/com/learning/eCommerce/controller/customerController/CustomerController.java
+++ eCommerce/src/main/java/com/learning/eCommerce/controller/customerController/CustomerController.java
@@ -37,7 +37,7 @@ public class CustomerController {
     }
 
     // GET BY ID
-    @GetMapping("/{id}")
+    @GetMapping("/{getCustomerById}")
     public ResponseEntity<CustomerResponseDTO> getCustomerById(@PathVariable Long id) {
         CustomerResponseDTO responseDTO = customerService.getCustomerById(id);
         return ResponseEntity.ok(responseDTO);
