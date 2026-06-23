c0f796f8b2146a4252fff8b030a6d7686e017e3b
manu-005 <manojbetadur@gmail.com>
2026-06-23 23:48:21 +0530

    product repository

--- eCommerce/src/main/java/com/learning/eCommerce/repository/ProductRepository.java
+++ eCommerce/src/main/java/com/learning/eCommerce/repository/ProductRepository.java
@@ -3,7 +3,11 @@ package com.learning.eCommerce.repository;
 import com.learning.eCommerce.entity.products.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    Optional<ProductEntity> findBySku(String sku);
 
     boolean existsBySku(String sku);
 }
