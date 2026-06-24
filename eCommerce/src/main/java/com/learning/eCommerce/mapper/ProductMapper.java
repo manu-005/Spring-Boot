ad50b502baaab4b29a0b7421bf4ad641b07b1e5c
manu-005 <manojbetadur@gmail.com>
2026-06-25 02:14:49 +0530

    update product

--- eCommerce/src/main/java/com/learning/eCommerce/mapper/ProductMapper.java
+++ eCommerce/src/main/java/com/learning/eCommerce/mapper/ProductMapper.java
@@ -4,6 +4,7 @@ import com.learning.eCommerce.dto.productsDTO.ProductRequestDto;
 import com.learning.eCommerce.dto.productsDTO.ProductResponseDto;
 import com.learning.eCommerce.entity.products.ProductEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
@@ -11,4 +12,10 @@ public interface ProductMapper {
     ProductEntity toEntity(ProductRequestDto dto);
 
     ProductResponseDto toResponseDto(ProductEntity entity);
+
+    void updateProductFromDto(
+            ProductRequestDto dto,
+            @MappingTarget ProductEntity entity
+    );
+
 }
