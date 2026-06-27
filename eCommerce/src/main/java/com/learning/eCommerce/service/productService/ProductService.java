b2fb4ea97861d73219361705d8cddee41670fb4e
manu-005 <manojbetadur@gmail.com>
2026-06-27 21:13:17 +0530

    update product

--- eCommerce/src/main/java/com/learning/eCommerce/service/productService/ProductService.java
+++ eCommerce/src/main/java/com/learning/eCommerce/service/productService/ProductService.java
@@ -69,7 +69,6 @@ public class ProductService implements ProductServiceInterface {
 
             entity.setImages(images);
         }
-
         ProductEntity savedProduct = productRepository.save(entity);
         return productMapper.toResponseDto(entity);
     }
