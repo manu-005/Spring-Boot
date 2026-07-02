17e855677f90d477f10218c8f7281042bc3f752e
manu-005 <manojbetadur@gmail.com>
2026-07-02 23:47:28 +0530

    cart entity

--- eCommerce/src/main/java/com/learning/eCommerce/entity/cartEntity/CartEntity.java
+++ eCommerce/src/main/java/com/learning/eCommerce/entity/cartEntity/CartEntity.java
@@ -1,4 +1,27 @@
 package com.learning.eCommerce.entity.cartEntity;
 
+import com.learning.eCommerce.entity.customerEntity.CustomerEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "cartEntity")
 public class CartEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Long cartId;
+
+    @OneToOne
+    @JoinColumn(name = "customerId", nullable = false)
+    private CustomerEntity customer;
+
+    @Column(name = "total_price")
+    private Double totalPrice = 0.0;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItemEntity> cartItems = new ArrayList<>();
+
 }
